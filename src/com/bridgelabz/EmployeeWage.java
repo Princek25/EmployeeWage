26360306bfc4c795938f4d3d85e42e33c3be2d09
Prince <princek2530@gmail.com>
2022-11-27 10:14:03 +0530

    Added: Calculating Employee Wage for a Month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -6,24 +6,30 @@ public class EmployeeWage {
     static final int PART_TIME_HOURS = 4;
     static final int IS_FULL_TIME = 2;
     static final int IS_PART_TIME = 1;
+    static final int WORKING_DAYS_PER_MONTH = 20;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program on Main Branch");
-        int empCheck = (int)Math.floor(Math.random()*10)%3;
-        System.out.println(empCheck);
-        int empWage = 0;
+        int dayCount = 1;
+        while (dayCount<=WORKING_DAYS_PER_MONTH) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            System.out.println(empCheck);
+            int empWage = 0;
 
-        switch (empCheck){
-            case IS_FULL_TIME:
-                System.out.println("Employee is Full Time");
-                empWage = WAGE_PER_HOUR*FULL_DAY_HOURS;
-                break;
-            case IS_PART_TIME:
-                System.out.println("Employee is Part Time");
-                empWage = WAGE_PER_HOUR*PART_TIME_HOURS;
-                break;
-            default: System.out.println("Employee is Absent");
+            switch (empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is Full Time");
+                    empWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee is Part Time");
+                    empWage = WAGE_PER_HOUR * PART_TIME_HOURS;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+            }
+            System.out.println("Employee Wage for Day" + dayCount + " :- " + empWage);
+            dayCount++;
         }
-        System.out.println("Employee Wage:- " + empWage);
     }
 }
